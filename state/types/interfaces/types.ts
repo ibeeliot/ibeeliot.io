b767099a957df4b7d145062556eeeb5797e0cf76
ibe.eliot <41167569+ibeeliot@users.noreply.github.com>
2020-04-14 02:20:04 -0700

    Created universal types file to import all types from

---
+++ state/types/interfaces/types.ts
@@ -0,0 +1,30 @@
+// blog types
+export type Author = {
+  name: string;
+  phone: string;
+  shortbio: string;
+  title: string;
+  email: string;
+  company: string;
+  twitter: string;
+  facebook: string;
+  github: string;
+};
+
+export type HeroImage = {
+  imageUrl: string;
+  description: string;
+  title: string;
+};
+
+export type BlogPost = {
+  id: string;
+  body: string;
+  description: string;
+  publishedDate: string;
+  slug: string;
+  tags: Array<string>;
+  title: string;
+  heroImage?: HeroImage;
+  author?: Author;
+};
